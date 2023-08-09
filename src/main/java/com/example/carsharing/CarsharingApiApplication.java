dd47a791a46d485bca21f8f13830d5d8bdd906d2
Lukasz <taralalukasz123@gmail.com>
2023-08-09 19:32:10 +0200

    repo init

---
+++ src/main/java/com/example/carsharing/CarsharingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.carsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsharingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsharingApiApplication.class, args);
+	}
+
+}
